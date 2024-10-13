d3d48e3128ac2f0d957a8349091c692baeb50118
seominah <seominah95@gmail.com>
2024-10-13 15:05:44 +0900

    init porject

---
+++ src/main/java/com/pqu/PquApplication.java
@@ -0,0 +1,13 @@
+package com.pqu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PquApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PquApplication.class, args);
+  }
+
+}
